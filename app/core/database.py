74c272aed2827a0c2702d135fc087467f2fe539b
Davvio <mifanto02@gmail.com>
2025-08-08 00:30:01 +0200

    PostgreDB

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
+from sqlalchemy.orm import declarative_base
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+engine = create_engine(
+    DATABASE_URL, pool_pre_ping=True, future=True
+)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
